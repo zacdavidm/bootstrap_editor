edd316140ae2bc78b818bd4858b40dff2ecbe16e
ZacDavidM <zacdavidm@gmail.com>
2015-12-07 17:15:39 -0500

    first commit. clone foundation_editor, change to bootstrap, and implement bootstrap styles

---
+++ README.md
@@ -0,0 +1,4 @@
+bootstrap_editor
+=================
+
+Drupal module to for using wysiwyg editor with zurb bootstrap
---
+++ js/ckeditor_styles.js
@@ -0,0 +1,157 @@
+CKEDITOR.stylesSet.add('bootstrap_editor_styleset', [
+  {
+    name:'Inline List',
+    element:'ul',
+    attributes:{class:'list-inline'}
+  },
+  {
+    name:'Unstyled List',
+    element:'ul',
+    attributes:{class:'list-unstyled'}
+  },
+  {
+    name:'Lead Paragraph',
+    element:'p',
+    attributes:{class:'lead'}
+  },
+  {
+    name:'16x9 Responsive Embed',
+    element:'div',
+    attributes:{class:'embed-responsive embed-responsive-16by9'}
+  },
+  {
+    name:'4x3 Responsive Embed',
+    element:'div',
+    attributes:{class:'embed-responsive embed-responsive-4by3'}
+  },
+  {
+    name:'Well',
+    element:'div',
+    attributes: {class:'well'}
+  },
+  {
+    name:'Well Large',
+    element:'div',
+    attributes: {class:'well well-lg'}
+  },
+  {
+    name:'Well Small',
+    element:'div',
+    attributes: {class:'well well-sm'}
+  },
+  {
+    name:'Row',
+    element:'div',
+    attributes: {class:'row'}
+  },
+  {
+    name:'Medium-1 Column',
+    element:'div',
+    attributes: {class:'col-md-1'}
+  },
+  {
+    name:'Medium-2 Column',
+    element:'div',
+    attributes: {class:'col-md-2'}
+  },
+  {
+    name:'Medium-3 Column',
+    element:'div',
+    attributes: {class:'col-md-3'}
+  },
+  {
+    name:'Medium-4 Column',
+    element:'div',
+    attributes: {class:'col-md-4'}
+  },
+  {
+    name:'Medium-5 Column',
+    element:'div',
+    attributes: {class:'col-md-5'}
+  },
+  {
+    name:'Medium-6 Column',
+    element:'div',
+    attributes: {class:'col-md-6'}
+  },
+  {
+    name:'Medium-7 Column',
+    element:'div',
+    attributes: {class:'col-md-7'}
+  },
+  {
+    name:'Medium-8 Column',
+    element:'div',
+    attributes: {class:'col-md-8'}
+  },
+  {
+    name:'Medium-9 Column',
+    element:'div',
+    attributes: {class:'col-md-9'}
+  },
+  {
+    name:'Medium-10 Column',
+    element:'div',
+    attributes: {class:'col-md-10'}
+  },
+  {
+    name:'Medium-11 Column',
+    element:'div',
+    attributes: {class:'col-md-11'}
+  },
+  {
+    name:'Medium-12 Column',
+    element:'div',
+    attributes: {class:'col-md-12'}
+  },
+  {
+    name:'Clear Block Elements',
+    element:['div', 'p'],
+    attributes: {class:'clear'}
+  },
+  {
+    name:'Float Left',
+    element:['div', 'p'],
+    attributes: {class:'pull-left'}
+  },
+  {
+    name:'Float Right',
+    element:['div', 'p'],
+    attributes: {class:'pull-right'}
+  },
+  {
+    name: 'Button Default',
+    element: 'a',
+    attributes: {class: 'btn btn-default'}
+  },
+  {
+    name: 'Button Primary',
+    element: 'a',
+    attributes: {class: 'btn btn-primary'}
+  },
+  {
+    name: 'Extra Small Button',
+    element: 'a',
+    attributes: {class: 'btn btn-default btn-xs'}
+  },
+  {
+    name: 'Small Button',
+    element: 'a',
+    attributes: {class: 'btn btn-default btn-sm'}
+  },
+  {
+    name: 'Large Button',
+    element: 'a',
+    attributes: {class: 'btn btn-default btn-lg'}
+  },
+  {
+    name: 'Block Level Button',
+    element: 'a',
+    attributes: {class: 'btn btn-default btn-block'}
+  },
+  {
+    name: 'Quote Block',
+    element: 'blockquote',
+    attributes: {class: 'quote-block'}
+  }
+]);
